1075ef9c3edc57cca93cd7de7b0adf2fbbf07914
webrectoversocreationbe <web@rectoversocreation.be>
2013-12-12 11:28:55 +0100

    66

--- www/DB.js
+++ www/DB.js
@@ -6,18 +6,135 @@ var bDoLogin=false;
 */
 function Init() {
 	log('Initialisation');
-	dbu.initialize(function() {
-		if (tableUserOk==true) {bDoLogin=true;}
-		if (bDoLogin==true) {
-			$('#Init').removeClass('current');
-			$('#Connexion').addClass('current');
-			$('#User').focus();
-		}
+	dbsync.initialize(function() {
+		dbu.initialize(function() {
+			if (tableUserOk==true && tableSynchroOk==true) {bDoLogin=true;}
+			if (bDoLogin==true) {
+				$('#Init').removeClass('current');
+				$('#Connexion').addClass('current');
+				$('#User').focus();
+			}
+		});
 	});
 }
 function SynchroAll() {
-	dbu.synchro();
+	if (bConnected==false) {
+		alert('Impossible de synchroniser sans connexion WiFi');
+	} else {
+		dbu.synchro();
+	}
 }
+/*
+	TABLE SYNCHRO
+*/
+window.dbsync = {
+	Etat: false,
+	bDoSynchro: false,
+	syncOK: false,
+    syncURL: "http://192.168.0.248/UDC/ajaxSync.php",
+    initialize: function(callback) {
+        var self = this;
+        this.db = window.openDatabase("syncdb", "1.0", "SyncDB", 20000000);
+        this.db.transaction(
+            function(tx) {
+                tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='Synchro'", this.txErrorHandler,
+                    function(tx, results) {
+                        if (results.rows.length == 1) {
+							self.Etat=true;
+                            log('La table Synchro existe');
+							tableUserOk=true;
+			                callback();
+                        } else {
+                            log('La table Synchro n\'existe pas');
+                            self.createTable(callback);
+                        }
+                    });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        this.db.transaction(
+            function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS Synchro (" +
+				"Genre VARCHAR(50) PRIMARY KEY, " +
+				"DateS VARCHAR(20), " +
+				"Nb INTEGER)"
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table Synchro à été créé');
+				tableUserOk=true;
+				self.initOk();
+                callback();
+            }
+        );
+    },
+	initOk: function() {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table de synchronisation créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro();}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+			callback: callback,
+            url: self.syncURL,
+	        crossDomain: true,
+			type: "POST",
+            data: {Genre: 'SYNCHRO'},
+            success:function (data) {
+				self.db.transaction(
+					function(tx) {
+						var sql = "delete from Synchro";
+						tx.executeSql(sql);
+					},
+					self.txErrorHandler,
+					function(tx) {
+					}
+				);
+				self.db.transaction(
+					function(tx) {
+						var l = data.length;
+						var sql = "INSERT OR REPLACE INTO Synchro (Genre,DateS,Nb) VALUES (?, ?, ?)";
+						var e;
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.Genre, e.DateS, e.Nb];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {
+						callback();
+					}
+				);
+				log('La table Synchro à été synchronisée');
+				self.Etat=true;
+				self.syncOK=true;
+				tableUserOk=true;
+				callback();
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        });
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL '+tx.message);
+    }
+};
 /*
 	TABLE USERS
 */
