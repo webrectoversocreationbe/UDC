82b2d3d3c6b9047e39f2dcceb86dcbab5cff449f
webrectoversocreationbe <web@rectoversocreation.be>
2013-12-12 14:52:59 +0100

    93

--- www/DB.js
+++ www/DB.js
@@ -1,5 +1,6 @@
 var tableUserOk=false;
 var tableSynchroOk=false;
+var tableModeleOk=false;
 var bDoLogin=false;
 var madb;
 /* 
@@ -10,12 +11,14 @@ function Init() {
 	madb=window.openDatabase("syncdb", "1.0", "SyncDB", 20000000);
 	dbsync.initialize(function(){
 		dbu.initialize(function(){
-			if (tableUserOk==true && tableSynchroOk==true) {bDoLogin=true;}
-			if (bDoLogin==true) {
-				$('#Init').removeClass('current');
-				$('#Connexion').addClass('current');
-				$('#User').focus();
-			}
+			dbmod.initialize(function(){
+				if (tableUserOk==true && tableSynchroOk==true && tableModeleOk==true) {bDoLogin=true;}
+				if (bDoLogin==true) {
+					$('#Init').removeClass('current');
+					$('#Connexion').addClass('current');
+					$('#User').focus();
+				}
+			});
 		});
 	});
 }
@@ -67,7 +70,7 @@ window.dbsync = {
             this.txErrorHandler,
             function() {
                 log('La table Synchro à été créé');
-				tableUserOk=true;
+				tableSynchroOk=true;
 				self.initOk(callback);
             }
         );
@@ -280,5 +283,116 @@ window.dbu = {
     txErrorHandler: function(tx) {
         alert(tx.message);
 		log('Erreur SQL User '+tx.message);
+    }
+};
+/*
+	TABLE MODELES
+*/
+window.dbmod = {
+	Etat: false,
+	bDoSynchro: false,
+	syncOK: false,
+    syncURL: "http://192.168.0.248/UDC/ajaxSync.php",
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(
+            function(tx) {
+                tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='Mods'", this.txErrorHandler,
+                    function(tx, results) {
+                        if (results.rows.length == 1) {
+							self.Etat=true;
+                            log('La table Modèles existe');
+							tableModeleOk=true;
+			                callback();
+                        } else {
+                            log('La table Modèles n\'existe pas');
+                            self.createTable(callback);
+                        }
+                    });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(
+            function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS Mods (" +
+				"MODNR VARCHAR(6) PRIMARY KEY, " +
+				"MOUC VARCHAR(100), " +
+				"MOCOEF REAL, " +
+				"MODELAI INTEGER, " +
+				"FOUR VARCHAR(3), " +
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table Modèles à été créé');
+				tableModeleOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table de modèles créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: self.syncURL,
+	        crossDomain: true,
+			async: false,
+			type: "POST",
+            data: {Genre: 'MOD'},
+            success:function (data) {
+				madb.transaction(
+					function(tx) {
+						var sql = "delete from Mods";
+						tx.executeSql(sql);
+					},
+					self.txErrorHandler,
+					function(tx) {
+					}
+				);
+				madb.transaction(
+					function(tx) {
+						var l = data.length;
+						var sql = "INSERT OR REPLACE INTO Mods (MODNR,MOUC,MOCOEF,MODELAI,FOUR) VALUES (?, ?, ?, ?, ?)";
+						var e;
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.MODNR, e.MOUC, e.MOCOEF, e.MODELAI, e.FOUR];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {
+					}
+				);
+				self.Etat=true;
+				self.syncOK=true;
+				tableModeleOk=true;
+				log('La table Modèles à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL Sync '+tx.message);
     }
 };
