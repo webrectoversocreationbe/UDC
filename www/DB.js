09200d6dd3d51f008d45da7bb5843b0e70bbc753
webrectoversocreationbe <web@rectoversocreation.be>
2013-12-13 10:52:50 +0100

    105

--- www/DB.js
+++ www/DB.js
@@ -4,6 +4,11 @@ var tableSynchroOk=false;
 var tableModeleOk=false;
 var tableCuirModOk=false;
 var tableLiasCuirOk=false;
+var tableLiasColoOk=false;
+var tableOptiModOk=false;
+var tableLiasOptiOk=false;
+var tableEleModOk=false;
+var tableElementOk=false;
 var tablePrixOk=false;
 var bDoLogin=false;
 var madb;
@@ -14,22 +19,25 @@ function Init() {
 	log('Initialisation');
 	madb=window.openDatabase("syncdb", "1.0", "SyncDB", 20000000);
 	dbsync.initialize(function(){
-		dbu.initialize(function(){
-			dbmod.initialize(function(){
-				dbcuirmod.initialize(function(){
-					dbliascuir.initialize(function(){
-						dbprix.initialize(function(){
-							if (tableUserOk==true && tableSynchroOk==true && tableModeleOk==true && tableCuirModOk==true && tableLiasCuirOk==true && tablePrixOk==true) {bDoLogin=true;}
-							if (bDoLogin==true) {
-								$('#Init').removeClass('current');
-								$('#Connexion').addClass('current');
-								$('#User').focus();
-							}
-						});
-					});
-				});
-			});
-		});
+	 dbu.initialize(function(){
+	  dbmod.initialize(function(){
+	   dbcuirmod.initialize(function(){
+		dbliascuir.initialize(function(){
+		 dbliascolo.initialize(function(){
+		  dboptimod.initialize(function(){
+		   dbliasopti.initialize(function(){
+		    dbelemod.initialize(function(){
+		     dbelement.initialize(function(){
+		      dbprix.initialize(function(){
+					if (tableUserOk==true && tableSynchroOk==true && tableModeleOk==true && tableCuirModOk==true && tableLiasCuirOk==true 
+						 && tableLiasColoOk==true && tableOptiModOk==true && tableLiasOptiOk==true && tableEleModOk==true && tableElementOk==true && tablePrixOk==true
+						) {bDoLogin=true;}
+					if (bDoLogin==true) {
+						$('#Init').removeClass('current');
+						$('#Connexion').addClass('current');
+						$('#User').focus();
+					}
+				});	});	});	});	});	}); });	}); });	});
 	});
 }
 function SynchroAll() {
@@ -423,32 +431,24 @@ window.dbmod = {
 	TABLE CUIRMOD
 */
 window.dbcuirmod = {
-	Etat: false,
-	bDoSynchro: false,
-	syncOK: false,
+	Etat: false, bDoSynchro: false,	syncOK: false,
     initialize: function(callback) {
         var self = this;
-        madb.transaction(
-            function(tx) {
-                tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='CuirMod'", this.txErrorHandler,
-                    function(tx, results) {
-                        if (results.rows.length == 1) {
-							self.Etat=true;
-                            log('La table CuirMod existe');
-							tableCuirModOk=true;
-			                callback();
-                        } else {
-                            log('La table CuirMod n\'existe pas');
-                            self.createTable(callback);
-                        }
-                    });
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='CuirMod'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table CuirMod existe');	tableCuirModOk=true;
+					callback();
+				} else {
+					log('La table CuirMod n\'existe pas'); self.createTable(callback);
+				}
+             });
             }
         )
     },
     createTable: function(callback) {
         var self = this;
-        madb.transaction(
-            function(tx) {
+        madb.transaction(function(tx) {
 				var sql = 
 				"CREATE TABLE IF NOT EXISTS CuirMod (" +
 				"MODNR VARCHAR(6), " +
@@ -509,7 +509,7 @@ window.dbcuirmod = {
 	},
     txErrorHandler: function(tx) {
         alert(tx.message);
-		log('Erreur SQL Sync '+tx.message);
+		log('Erreur SQL CuirMod '+tx.message);
     }
 };
 /*
@@ -602,6 +602,429 @@ window.dbliascuir = {
     txErrorHandler: function(tx) {
         alert(tx.message);
 		log('Erreur SQL Sync '+tx.message);
+    }
+};
+/*
+	TABLE LIASCOLO
+*/
+window.dbliascolo = {
+	Etat: false, bDoSynchro: false,	syncOK: false,
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='LiasColo'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table LiasColo existe');	tableLiasColoOk=true;
+					callback();
+				} else {
+					log('La table LiasColo n\'existe pas'); self.createTable(callback);
+				}
+             });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS LiasColo (" +
+				"FOURN VARCHAR(3), " +
+				"CUIRNR VARCHAR(6), " +
+				"COLORNR VARCHAR(4), " +
+				"COLOUC VARCHAR(30))";
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table LiasColo à été créé');
+				tableLiasColoOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table de Couleurs créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: syncURL, crossDomain: true, async: false, type: "POST", data: {Genre: 'LIASCOLO'},
+            success:function (data) {
+				madb.transaction(function(tx) {var sql = "delete from LiasColo"; tx.executeSql(sql);},self.txErrorHandler,function(tx) {});
+				madb.transaction(
+					function(tx) {
+						var l = data.length; var e;
+						var sql = "INSERT OR REPLACE INTO LiasColo (FOURN,CUIRNR,COLORNR,COLOUC) VALUES (?, ?, ?, ?)";
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.FOURN, e.CUIRNR, e.COLORNR, e.COLOUC];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {}
+				);
+				self.Etat=true;	self.syncOK=true;
+				tableLiasColoOk=true;
+				log('La table LiasColo à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL LiasColo '+tx.message);
+    }
+};
+/*
+	TABLE OPTIMOD
+*/
+window.dboptimod = {
+	Etat: false, bDoSynchro: false,	syncOK: false,
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='OptiMod'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table OptiMod existe');	tableLOptiModOk=true;
+					callback();
+				} else {
+					log('La table OptiMod n\'existe pas'); self.createTable(callback);
+				}
+             });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS OptiMod (" +
+				"MODNR VARCHAR(6), " +
+				"OPCODE VARCHAR(3))";
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table OptiMod à été créé');
+				tableOptiModOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table options de modèles créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: syncURL, crossDomain: true, async: false, type: "POST", data: {Genre: 'OPTIMOD'},
+            success:function (data) {
+				madb.transaction(function(tx) {var sql = "delete from OptiMod"; tx.executeSql(sql);},self.txErrorHandler,function(tx) {});
+				madb.transaction(
+					function(tx) {
+						var l = data.length; var e;
+						var sql = "INSERT OR REPLACE INTO OptiMod (MODNR,OPCODE) VALUES (?, ?)";
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.MODNR, e.OPCODE];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {}
+				);
+				self.Etat=true;	self.syncOK=true;
+				tableOptiModOk=true;
+				log('La table OptiMod à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL OptiMod '+tx.message);
+    }
+};
+/*
+	TABLE LIASOPTI
+*/
+window.dbliasopti = {
+	Etat: false, bDoSynchro: false,	syncOK: false,
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='LiasOpti'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table LiasOpti existe');	tableLiasOptiOk=true;
+					callback();
+				} else {
+					log('La table LiasOpti n\'existe pas'); self.createTable(callback);
+				}
+             });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS LiasOpti (" +
+				"FOUR VARCHAR(3), " +
+				"OPCODE VARCHAR(3), " +
+				"OPFR VARCHAR(30))";
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table LiasOpti à été créé');
+				tableLiasOptiOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table de description d\'option créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: syncURL, crossDomain: true, async: false, type: "POST", data: {Genre: 'LIASOPTI'},
+            success:function (data) {
+				madb.transaction(function(tx) {var sql = "delete from LiasOpti"; tx.executeSql(sql);},self.txErrorHandler,function(tx) {});
+				madb.transaction(
+					function(tx) {
+						var l = data.length; var e;
+						var sql = "INSERT OR REPLACE INTO LiasOpti (FOUR,OPCODE,OPFR) VALUES (?, ?, ?)";
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.FOUR, e.OPCODE, e.OPFR];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {}
+				);
+				self.Etat=true;	self.syncOK=true;
+				tableLiasOptiOk=true;
+				log('La table LiasOpti à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL LiasOpti '+tx.message);
+    }
+};
+/*
+	TABLE ELEMOD
+*/
+window.dbelemod = {
+	Etat: false, bDoSynchro: false,	syncOK: false,
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='EleMod'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table EleMod existe');	tableLEleModOk=true;
+					callback();
+				} else {
+					log('La table EleMod n\'existe pas'); self.createTable(callback);
+				}
+             });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS EleMod (" +
+				"MODNR VARCHAR(6), " +
+				"ELCODE VARCHAR(7))";
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table EleMod à été créé');
+				tableEleModOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table éléments de modèles créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: syncURL, crossDomain: true, async: false, type: "POST", data: {Genre: 'ELEMOD'},
+            success:function (data) {
+				madb.transaction(function(tx) {var sql = "delete from EleMod"; tx.executeSql(sql);},self.txErrorHandler,function(tx) {});
+				madb.transaction(
+					function(tx) {
+						var l = data.length; var e;
+						var sql = "INSERT OR REPLACE INTO EleMod (MODNR,ELCODE) VALUES (?, ?)";
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.MODNR, e.ELCODE];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {}
+				);
+				self.Etat=true;	self.syncOK=true;
+				tableEleModOk=true;
+				log('La table EleMod à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL EleMod '+tx.message);
+    }
+};
+/*
+	TABLE ELEMENT
+*/
+window.dbelement = {
+	Etat: false, bDoSynchro: false,	syncOK: false,
+    initialize: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+             tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='Element'", this.txErrorHandler, function(tx, results) {
+				if (results.rows.length == 1) {
+					self.Etat=true;	log('La table Element existe');	tableElementOk=true;
+					callback();
+				} else {
+					log('La table Element n\'existe pas'); self.createTable(callback);
+				}
+             });
+            }
+        )
+    },
+    createTable: function(callback) {
+        var self = this;
+        madb.transaction(function(tx) {
+				var sql = 
+				"CREATE TABLE IF NOT EXISTS Element (" +
+				"ELCODE VARCHAR(7), " +
+				"ELFR VARCHAR(40))";
+                tx.executeSql(sql);
+            },
+            this.txErrorHandler,
+            function() {
+                log('La table Element à été créé');
+				tableElementOk=true;
+				self.initOk(callback);
+            }
+        );
+    },
+	initOk: function(callback) {
+        var self = this;
+		if (self.Etat==false) {
+			$('#InitResult').append('Table de description d\'élément créée<br/>');
+			if (bConnected==false) {
+				$('#InitResult').append('Il faut synchroniser avec le serveur<br/>Vous n\'êtes pas connecté<br/><a onclick="Init()" class="rouge">Réessayer</a>');
+			} else {
+				self.bDoSynchro=true;
+			}
+		}
+		if (self.bDoSynchro==true) {self.synchro(callback);}
+	},
+	synchro: function(callback) {
+        var self = this;
+        $.ajax({
+            url: syncURL, crossDomain: true, async: false, type: "POST", data: {Genre: 'ELEMENT'},
+            success:function (data) {
+				madb.transaction(function(tx) {var sql = "delete from Element"; tx.executeSql(sql);},self.txErrorHandler,function(tx) {});
+				madb.transaction(
+					function(tx) {
+						var l = data.length; var e;
+						var sql = "INSERT OR REPLACE INTO Element (ELCODE,ELFR) VALUES (?, ?)";
+						for (var i = 0; i < l; i++) {
+							e = data[i];
+							var params = [e.ELCODE, e.ELFR];
+							tx.executeSql(sql, params);
+						}
+					},
+					self.txErrorHandler,
+					function(tx) {}
+				);
+				self.Etat=true;	self.syncOK=true;
+				tableElementOk=true;
+				log('La table Element à été synchronisée');
+            },
+            error: function(request, model, response) {
+				log('Erreur durant la synchronisation');
+                alert(request.responseText + " " +model + " " + response);
+            }
+        }).done(function() {
+			callback();
+		});
+	},
+    txErrorHandler: function(tx) {
+        alert(tx.message);
+		log('Erreur SQL Element '+tx.message);
     }
 };
 /*
