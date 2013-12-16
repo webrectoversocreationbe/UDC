1b75aa7074f1368187dacdf219ec782dea4aea79
webrectoversocreationbe <web@rectoversocreation.be>
2013-12-16 09:03:38 +0100

    135

---
+++ www/util.js
@@ -0,0 +1,50 @@
+function repeatString(str, num) {out = ''; for (var i = 0; i < num; i++) {out += str;} return out;}
+function dump(v, howDisplay, recursionLevel) {
+    howDisplay = (typeof howDisplay === 'undefined') ? "alert" : howDisplay;
+    recursionLevel = (typeof recursionLevel !== 'number') ? 0 : recursionLevel;
+    var vType = typeof v; var out = vType;
+    switch (vType) {
+        case "number":
+        case "boolean":
+            out += ": " + v;
+            break;
+        case "string":
+            out += "(" + v.length + '): "' + v + '"';
+            break;
+        case "object":
+            if (v === null) {out = "null";}
+            else if (Object.prototype.toString.call(v) === '[object Array]') {  
+                out = 'array(' + v.length + '): {\n';
+                for (var i = 0; i < v.length; i++) {
+                    out += repeatString('   ', recursionLevel) + "   [" + i + "]:  " + 
+                        dump(v[i], "none", recursionLevel + 1) + "\n";
+                }
+                out += repeatString('   ', recursionLevel) + "}";
+            }
+            else {
+                sContents = "{\n";
+                cnt = 0;
+                for (var member in v) {
+                    sContents += repeatString('   ', recursionLevel) + "   " + member +
+                        ":  " + dump(v[member], "none", recursionLevel + 1) + "\n";
+                    cnt++;
+                }
+                sContents += repeatString('   ', recursionLevel) + "}";
+                out += "(" + cnt + "): " + sContents;
+            }
+            break;
+    }
+    if (howDisplay == 'body') {
+        var pre = document.createElement('pre');
+        pre.innerHTML = out;
+        document.body.appendChild(pre)
+    }
+    else if (howDisplay == 'alert') {
+        alert(out);
+    } else {
+		if ($('#'+howDisplay)) {
+			$('#'+howDisplay).prepend(out);
+		}
+	}
+    return out;
+}
