9cd16e0805b018bc36703d05fef2a096636c1cf6
webrectoversocreationbe <web@rectoversocreation.be>
2013-12-17 16:06:13 +0100

    193

--- www/DB.js
+++ www/DB.js
@@ -1228,7 +1228,7 @@ Modele.prototype = {
 				//Chercher le prix de chaque element
 				for (var cpt=0;cpt<self.Elements.length;cpt++) {
 					var elcode=self.Elements[cpt].ELCODE;
-					function (value) {
+					function(value) {
 						madb.transaction(
 							function(tx) {
 								var sql = "SELECT PRIX FROM Prix where MODNR='"+self.MODNR+"' and PXCATEG='"+self.CUCAT+"' and PXELEM='"+elcode+"'";
