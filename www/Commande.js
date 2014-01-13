232f98fb20322199e1f07421000c83ecf1a54b4c
webrectoversocreationbe <web@rectoversocreation.be>
2014-01-13 13:10:52 +0100

    31

--- www/Commande.js
+++ www/Commande.js
@@ -1,8 +1,4 @@
 function InitCommande() {
-	navigator.globalization.dateToString(
-		new Date(),
-		function (date) { alert('date: ' + date.value + '\n'); },
-		function () { alert('Error getting dateString\n'); },
-		{ formatLength: 'short', selector: 'date and time' }
-	);
+	var d=new Date();
+	alert(d.getDate());
 }
